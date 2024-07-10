10dba09042992a24aba3c3ab829502e5178e84e1
AbdouGRexe <110488900+AbdouGRexe@users.noreply.github.com>
2024-07-10 14:34:22 +0100

    pass stage

--- app/main.py
+++ app/main.py
@@ -5,16 +5,18 @@ def handle_http_requests(conn, addr):
     while True:
         request_msg = str(conn.recv(1024).decode())
         request_parts = request_msg.split(sep='\r\n')
+        print(f"requst parts: {request_parts}")
         
         start_line = request_parts[0]
         start_line = start_line.split(sep='/')
         
-        if start_line[0] in ['GET', 'POST', 'HEAD', 'OPTIONS']:
+        print(f"start line parts: {start_line}")
+        if start_line[0] in ['GET ', 'POST ', 'HEAD ', 'OPTIONS ']:
             path = start_line[1].split(sep=' ')[0]
             if path != '':
-                conn.send('HTTP/1.1 404 Not Found\r\n\r\n')
+                conn.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())
             else:
-                conn.send('HTTP/1.1 200 OK\r\n\r\n')  
+                conn.send('HTTP/1.1 200 OK\r\n\r\n'.encode())  
             
 def main():
     with socket.create_server(("localhost", 4221)) as server_socket:
