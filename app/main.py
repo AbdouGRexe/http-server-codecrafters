f79b5681d9f3cf1278a2a4e79433a84e53c78403
AbdouGRexe <110488900+AbdouGRexe@users.noreply.github.com>
2024-07-10 14:10:13 +0100

    test if stage is passed

--- app/main.py
+++ app/main.py
@@ -1,12 +1,30 @@
 import socket
+import threading
 
-
+def handle_http_requests(conn, addr):
+    while True:
+        request_msg = str(conn.recv(1024).decode())
+        request_parts = request_msg.split(sep='\r\n')
+        
+        start_line = request_parts[0]
+        start_line = start_line.split(sep='/')
+        
+        if start_line[0] in ['GET', 'POST', 'HEAD', 'OPTIONS']:
+            path = start_line[1].split(sep=' ')[0]
+            if path != '/':
+                conn.send('HTTP/1.1 404 Not Found\r\n\r\n')
+            else:
+                conn.send('HTTP/1.1 200 OK\r\n\r\n')  
+            
 def main():
     with socket.create_server(("localhost", 4221)) as server_socket:
-        client_request = server_socket.accept() # wait for client
-        conn, addr = client_request
-        conn.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
-    
+        while True:
+            client_request = server_socket.accept() # wait for client
+            conn, addr = client_request
+            
+            # each machine has its HTTP requests handled by a thread
+            thread = threading.Thread(target=handle_http_requests, args=(conn, addr))
+            thread.start()    
     
 
 
