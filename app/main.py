6c6246cb6078de7281a0cd1d02c31f7527826e6f
AbdouGRexe <110488900+AbdouGRexe@users.noreply.github.com>
2024-07-26 18:10:49 +0100

    return a file stage

--- app/main.py
+++ app/main.py
@@ -1,55 +1,10 @@
+from io import TextIOWrapper
 import socket
 import threading
 import re
 
 FORMAT = 'utf-8'
 
-    
-def main():
-    with socket.create_server(("localhost", 4221)) as server_socket:
-        while True:
-            client_request = server_socket.accept() # wait for client
-            conn, addr = client_request
-
-            # each machine has its HTTP request handled by a thread
-            thread = threading.Thread(target=handle_client, args=(conn, addr))
-            thread.start()    
-
-if __name__ == "__main__":
-    main()  
-
-
-
-def handle_client(conn : socket.socket, addr):
-    request_msg : str = conn.recv(1024).decode(FORMAT)
-    if request_msg:
-        
-        parsed_request = parse_req(request_msg)
-        response: dict = get_response(parsed_request)
-        
-        status: int = response['status_code']
-        content = response['response_body']
-        context : tuple[int, str] = (len(content), response['content-type'])
-        
-        match status:
-            case 501:
-                message = "HTTP/1.1 501 Not Implemented\r\n\r\n"
-            case 404:
-                message = "HTTP/1.1 404 Not Found\r\n\r\n"
-            case 400:
-                message = "HTTP/1.1 400 Bad Request\r\n\r\n"
-            case 201:
-                message = "HTTP/1.1 201 Created\r\n\r\n"
-            case 200:
-                if not content:
-                    message = "HTTP/1.1 200 OK\r\n\r\n"
-                else:
-                    message = f"HTTP/1.1 200 OK\r\nContent-Type: {context[1]}\r\nContent-Length: {context[0]}\r\n\r\n{content}"            
-        try:
-            conn.send(message.encode(FORMAT))
-        except:
-            print("Something went wrong when a response was being sent, might be a connection error")        
-        
 
 def get_response(req : dict) -> dict[str, str, str]:
     
@@ -66,28 +21,35 @@ def get_response(req : dict) -> dict[str, str, str]:
     if req['method'] not in ['GET', 'POST', 'PUT' , 'HEAD', 'OPTIONS']:
         res_GET['status-code'] = 501 # unsupported method 
         return res_GET
-
-    headers = [str(key) for key in dict['headers'].keys()] 
+    
+    headers = [str(key) for key in req['headers'].keys()]
     url_paths = ['/']
     if req['method'] == 'GET':
         
         if str(req['target']) in url_paths:
             res_GET['status-code'] = 200
         
-        if str(req['target']).lower() in headers:
+        if str(req['target']).lower().strip('/') in headers:
             res_GET['status-code'] = 200
-            res_GET['response-body'] = req['headers'][req['target'].lower()]
+            res_GET['response-body'] = req['headers'][req['target'].lower().strip('/')]
             res_GET['content-type'] = 'text/plain'
         
         if str(req['target']).startswith('/echo'):
             res_GET['status-code'] = 200
             res_GET['response-body'] = req['target'].split('/')[2]
             res_GET['content-type'] = 'text/plain'
-            
         
-        if str(req['target']).startswith('/files'):
+        if str(req['target']).startswith('/files'):            
             res_GET['status-code'] = 200
-            res_GET['content-type'] = 'application/octet-stream'
+            f_path = req['target'].split('/')[2]
+              
+            try:
+                f : TextIOWrapper = open(f_path, "+rt")
+                res_GET['response-body'] = f.read()
+                res_GET['content-type'] = 'application/octet-stream'          
+            except OSError:
+                res_GET['status-code'] = 404
+                    
         
         return res_GET     
         
@@ -115,13 +77,68 @@ def parse_req(msg : str)-> dict[str, str, str, str]:
         if header == '' and count != 0: # if no header is present
                                         # an additional blank line is found so we skip it.
             output_dict["body"] = req_parts[count + 1]
-            return output_dict            
+            return output_dict         
         if len(req_parts) > 3: # 4 is the minimum size for a header-filled request
             output_dict['headers'][header.split(': ')[0].lower()] = header.split(': ')[1]
         count += 1
+        
+    
+
+def handle_client(conn : socket.socket, addr):
+    request_msg : str = conn.recv(1024).decode(FORMAT)
+    if request_msg:
+        
+        parsed_request = parse_req(request_msg)
+        response: dict = get_response(parsed_request)
+        
+        status: int = response['status-code']
+        content = response['response-body']
+        if content:
+            context : tuple[int, str] = (len(content), response['content-type'])
+        
+        match status:
+            case 501:
+                message = "HTTP/1.1 501 Not Implemented\r\n\r\n"
+            case 404:
+                message = "HTTP/1.1 404 Not Found\r\n\r\n"
+            case 400:
+                message = "HTTP/1.1 400 Bad Request\r\n\r\n"
+            case 201:
+                message = "HTTP/1.1 201 Created\r\n\r\n"
+            case 200:
+                if not content:
+                    message = "HTTP/1.1 200 OK\r\n\r\n"
+                else:
+                    message = f"HTTP/1.1 200 OK\r\nContent-Type: {context[1]}\r\nContent-Length: {context[0]}\r\n\r\n{content}"            
+        try:
+            conn.send(message.encode(FORMAT))
+        except:
+            print("Something went wrong when a response was being sent, might be a connection error")        
+        
+
+
     
 
                   
     
+    
+def main():
+    with socket.create_server(("localhost", 4221)) as server_socket:
+        while True:
+            client_request = server_socket.accept() # wait for client
+            conn, addr = client_request
+
+            # each machine has its HTTP request handled by a thread
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
+            
+            
+
+if __name__ == "__main__":
+    main()  
+
+
+
+
          
 
